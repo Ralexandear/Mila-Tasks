3525c2686f3829c5efd735340d5396c70965a9d3
Aleksandr Ryzhov <ralexandear@gmail.com>
2024-10-29 20:03:01 +0100

    created first task

---
+++ task 1/index.js
@@ -0,0 +1,23 @@
+const firstEvent = {
+    date: new Date(2024, 5, 12), //дата события
+    participantIds: [1, 2, 3, 4] //id участников события
+}
+
+const secondEvent = {
+    date: new Date(2024, 8, 27), //дата события
+    participantIds: [1, 2, 3, 4] //id участников события
+}
+
+
+/**
+ * 
+ * @param {typeof firstEvent} firstEvent 
+ * @param {typeof secondEvent} secondEvent 
+ */
+function eventComparion(firstEvent, secondEvent) {
+    /**
+     * TODO
+     * Написать функцию которая дает в консоль ответ какое из событий случится раньше     
+     * И ответит на вопрос - одинаковые ли списки участников событий или нет
+     */
+}
